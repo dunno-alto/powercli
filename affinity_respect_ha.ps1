042cd0e8408b0d15e5f13264d1bd13b14f0d8501
alto <34450932+dunno-alto@users.noreply.github.com>
2026-05-22 10:26:17 -0700

    Update cluster name in affinity_respect_ha.ps1

--- affinity_respect_ha.ps1
+++ affinity_respect_ha.ps1
@@ -1,3 +1,3 @@
-$cluster = Get-Cluster -Name WTIA-VRTX
+$cluster = Get-Cluster -Name <clustername>
 New-AdvancedSetting -Entity $cluster -Type ClusterHA -Name 'das.respectvmvmantiaffinityrules' -Value false -Confirm:$false
 New-AdvancedSetting -Entity $cluster -Type ClusterHA -Name 'das.respectvmhostsoftaffinityrules' -Value false -Confirm:$false
